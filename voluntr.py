fdda064958ae4fba8fd9b7ef355959b4e8c36f9e
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-10-03 19:50:45 -0700

    added voluntr.py file with bare flask skeleton

---
+++ voluntr.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect, render_template
+
+# app setup
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# TODO -- add route
+# projects to look in for ideas: (web-caesar, user-signup, hello-flask, flicklist-flask)
+
+
+
+
+# runs the app, always the last line
+app.run()
