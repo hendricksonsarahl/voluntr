6dad5420b6d36e33bd2c250c7fd9b99c707b37cc
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-10-05 20:31:04 -0700

    changed pw for db

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:helpinghands@localhost:8889/voluntr'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
