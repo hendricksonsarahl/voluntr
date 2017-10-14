cd8c6a9de71a8f1eb9219edc012a15b2d46eae33
Ryland Bell <ryland.bell@gmail.com>
2017-10-14 15:03:00 -0700

    add template for org to preview post

--- voluntr.py
+++ voluntr.py
@@ -57,13 +57,10 @@ def edit_opportunity():
     message = "<h1>Organizations can edit an individual opportunity, or remove it from the app here</h1>"
     return message
 
-
 @app.route("/org/opportunity", methods=['GET'])
 def show_opportunity():
     '''displays details about a specific volunteer opportunity, with option to edit/delete the opportunity''' 
-    message = "<h1>Organizations can view details of a volunteer opportunity here</h1>"
-    return message
-
+    return render_template('organization/preview.html', title="Voluntr | Preview Post")
 
 @app.route("/drop_create", methods=['GET'])
 def dropCreate():
