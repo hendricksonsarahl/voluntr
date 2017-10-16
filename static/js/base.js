25f8cbe6c9f044143a5ad86e254a673d3cdf6129
Ryland Bell <ryland.bell@gmail.com>
2017-10-16 14:04:34 -0700

    add coverage report to js unit tests

--- static/js/base.js
+++ static/js/base.js
@@ -21,9 +21,13 @@ function displayError(message) {
 
 //don't run this outside of a browser environment (e.g., when testing in Node)
 if (typeof window !== "undefined") {
+  
   // Code in this anonymous function is immediately invoked once this script loads:
   (function() {
-    console.log('base.js loaded');
+    
+
+
+
   })();
 }
 
