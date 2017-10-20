b5abc985ecfcb91ce62964860ab2d05069425c2b
Ryland Bell <ryland.bell@gmail.com>
2017-10-20 12:00:43 -0700

    add function to save opp data to browser localStorage

--- static/js/vol.js
+++ static/js/vol.js
@@ -1,13 +1,39 @@
+// add opportunities to a JSON-encoded localStorage array
+function saveOpportunity(){
 
+  //used when checking if opportunity had previously been saved
+  function checkId(obj) {
+    return obj.id === window.currentOpp.id;
+  }
+
+  //get the current stored value for savedOpps
+  var loadedStore = JSON.parse(localStorage.getItem("savedOpps"));
+
+  //if no local store has been created for savedOpps, create an empty array to hold it
+  if(!loadedStore) {
+    var newStore = JSON.stringify([])
+    localStorage.setItem("savedOpps", newStore);
+  }
+
+  //check to see if the opportunity has already been saved:
+  if(loadedStore.findIndex(checkId) === -1) {
+
+    //add the currentOpp to the savedOpps store
+    loadedStore.push(window.currentOpp);
+    localStorage.setItem("savedOpps", JSON.stringify(loadedStore));
+  }
+}
 
 //don't run this outside of a browser environment (e.g., when testing in Node)
 if (typeof window !== "undefined") {
   
   // Code in this anonymous function is immediately invoked once this script loads:
   (function() {
-    
-    console.log('ahoy');
-
+    var saveButton = document.getElementById('save-button');
+    saveButton.addEventListener('click', function(e){
+      e.preventDefault();
+      saveOpportunity();
+    });
 
   })();
 }
@@ -16,6 +42,6 @@ if (typeof window !== "undefined") {
 // The leading if statement should prevent this block from running in the browser.
 if (typeof module !== "undefined" && module.exports) {
   module.exports = {
-    
+    saveOpportunity: saveOpportunity
   };
 }
