76df57555649dbaaf8da541f4301c453ef8e8d7d
Ryland Bell <ryland.bell@gmail.com>
2017-10-24 09:32:31 -0700

    move OAuth validation check to separate helper function

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 import datetime
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 # datetime object formatting helpers
 ######################################
@@ -138,3 +140,25 @@ def get_category_class(category):
         return "disabilities"
     else:
         return "unknown"
+
+# OAuth
+######################
+
+# Check the validity of an OAuth token, and return a Google ID (if valid) or False
+def process_oauth_token(token):
+    try:
+        CLIENT_ID = "649609603099-g73psa76kgviat4mdh2ctt1pk8he11bb.apps.googleusercontent.com"
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+        
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+        print ('The userID for the OAuth token is %s'%(userid))
+        return userid
+
+    # TODO: Error handling
+    except ValueError:
+        # Invalid token
+        return
