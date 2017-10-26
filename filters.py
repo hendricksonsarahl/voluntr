63a4e04494e91a4cfdc5d142f4ef86e6373ae1d8
Ryland Bell <ryland.bell@gmail.com>
2017-10-26 08:18:43 -0700

    add pyzipcode3, set up zip/distance filter

--- filters.py
+++ filters.py
@@ -1,13 +1,18 @@
 from app import db
 from models.org import Opportunity
 from helpers import get_day
+from pyzipcode import ZipCodeDatabase
+
+zcdb = ZipCodeDatabase()
 
 class Filters():
 
-    def __init__(self, category="all", availability=["all"]):
+    def __init__(self, category="all", availability=["all"], zip="97232", distance=5):
 
         self.category = category
         self.availability = availability
+        self.zip = zip
+        self.distance = distance
 
     def search(self):
         if self.category == "all":
@@ -20,6 +25,8 @@ class Filters():
         if self.availability[0] != "all" and len(self.availability) != 7:
             opps = self.filter_by_days(opps)
 
+        opps = self.filter_by_zip(opps)
+
         return opps
 
     def filter_by_days(self, opps):
@@ -31,4 +38,14 @@ class Filters():
                 for j in range (len(self.availability)):
                     if get_day(opps[i].startDateTime) == self.availability[j]:
                         filtered = filtered + [opps[i]]
+        return filtered
+
+    def filter_by_zip(self, opps):
+        filtered = []
+        zips = zcdb.get_zipcodes_around_radius(self.zip, self.distance)
+        for i in range(len(opps)):
+            for j in range (len(zips)):
+                if opps[i].zipcode == zips[j].zip:
+                    filtered = filtered + [opps[i]]
+                
         return filtered
