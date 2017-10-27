79b3dcb98993dda1e145ad391f8a372d77abfb44
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-10-27 16:35:37 -0700

    Removed secret key, no longer needed

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
 app.config['SQLALCHEMY_ECHO'] = True
-app.secret_key = 'dgts027a46710270f5ab170a2dde79da4da52482f3d5gd10'
 
 db = SQLAlchemy(app)
