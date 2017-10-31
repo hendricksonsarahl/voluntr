9e3c0d635505591eeb849238ca0d633aaa91a1cf
Ryland Bell <ryland.bell@gmail.com>
2017-10-31 13:50:51 -0700

    use env variables to get SQLALCHEMY_DATABASE_URI

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
+app.config.from_envvar('DATABASE_URL')
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
