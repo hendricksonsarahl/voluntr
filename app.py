819543e4a3a2e02eada4ff60cc08aebdf77e9784
Ryland Bell <ryland.bell@gmail.com>
2017-11-02 08:46:00 -0700

    test env variables on heroku

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+if 'CLEARDB_DATABASE_URL' in os.environ:
+  print ('Found it!')
+else:
+  print ('No CLEARDB_DATABASE_URL!')
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
-app.config.from_envvar('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
