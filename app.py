0fc7da104b8435d66f33eae7028ef3ebdf389a66
Ryland Bell <ryland.bell@gmail.com>
2017-11-02 08:48:48 -0700

    set SQLALCHEMY_DATABASE_URI differently based on env

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 if 'CLEARDB_DATABASE_URL' in os.environ:
-  print ('Found it!')
+  db_uri = os.environ['CLEARDB_DATABASE_URL']
 else:
-  print ('No CLEARDB_DATABASE_URL!')
+  db_uri = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
+
+print ('db_uri = ', db_uri)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
