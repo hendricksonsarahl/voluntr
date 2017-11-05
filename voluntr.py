6f13fefd7e76af9db664b22055cdf87d5818faaf
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-11-04 23:11:32 -0700

    added nav to error page and cleaned up route using helper

--- voluntr.py
+++ voluntr.py
@@ -59,11 +59,11 @@ def opportunities():
 
         search = Filters(categories=categories, availability=availability, zipcode=zipcode, distance=distance) # creates filter with given category and availability
         opps = search.search() #grabs list of opportunities
-        if type(opps) == type("String"):
-            return render_template('volunteer/filter_errors.html', error=opps)
 
-        if len(opps) == 0:
-            return render_template('volunteer/filter_errors.html', error="No results. Try a less refined search.")
+        error = check_opps(opps)
+        if error:
+            return render_template('volunteer/filter_errors.html', error=error)
+        
         opp = opps[index] # picks out the opp at index
         index = increment(index, len(opps)) # increments index
         
