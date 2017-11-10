33488063defe792442246e50edeb091d8d3e9a86
Ryland Bell <ryland.bell@gmail.com>
2017-11-10 13:17:02 -0800

    bundle vendor code with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/js/main.js",
+  entry: {
+    bundle: "./src/js/main.js",
+    vendor: ["jquery", "bootstrap", "intro.js"]
+  },
   output: {
     path: path.resolve(__dirname, "static/build"),
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   module: {
     rules: [
@@ -25,5 +30,14 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ]
+  // plugins: [
+  //   new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js' })
+  // ]
 };
