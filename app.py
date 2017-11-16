13b2bdfdf4a175597a5459e4bc515e2a9e68a111
Ryland Bell <ryland.bell@gmail.com>
2017-11-16 12:17:28 -0800

    add env-specific config to app.py

--- app.py
+++ app.py
@@ -9,12 +9,21 @@ else:
   db_uri = 'mysql+pymysql://voluntr:voluntr@localhost:8889/voluntr'
 
 app = Flask(__name__)
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+
+# Settings for both production and development environments:
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 30 # required to keep connections to ClearDB from failing
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # re-load static assets every time
 app.secret_key = 'ZAj08N/$3m]XHjHy!rX R/~?X,9RW@UL'
+
+if 'IS_PRODUCTION' in os.environ:
+  # Production-only settings
+  app.config['DEBUG'] = False
+  app.config['SQLALCHEMY_POOL_RECYCLE'] = 30 # required to keep connections to ClearDB from failing
+
+else:
+  # Development-only settings
+  app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+  app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # re-load static assets every time
 
 db = SQLAlchemy(app)
