ffb12faf20158afd613efb410a0d0095c33124e8
Ryland Bell <ryland.bell@gmail.com>
2017-11-16 12:31:29 -0800

    check env variables for secret_key

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-# Check environment variables to determine Heroku vs. local database
+# Check env variables to determine Heroku vs. local database
 if 'CLEARDB_DATABASE_URL' in os.environ:
   db_uri = os.environ['CLEARDB_DATABASE_URL']
 else:
@@ -14,7 +14,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'ZAj08N/$3m]XHjHy!rX R/~?X,9RW@UL'
 
 if 'IS_PRODUCTION' in os.environ:
   # Production-only settings
@@ -25,5 +24,11 @@ else:
   # Development-only settings
   app.config['DEBUG'] = True      # displays runtime errors in the browser, too
   app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # re-load static assets every time
+
+# Check env variables to find an actually secret SECRET_KEY for Heroku deployment
+if 'SECRET_KEY' in os.environ:
+  app.secret_key = os.environ['SECRET_KEY']
+else:
+  app.secret_key = 'ZAj08N/$3m]XHjHy!rX R/~?X,9RW@UL'
 
 db = SQLAlchemy(app)
